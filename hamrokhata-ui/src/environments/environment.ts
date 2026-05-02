d557b21c950c5953e5f9c31d45db7b9f61ddd117
Rupak Pathak <pathakrupak00@gmail.com>
2026-05-02 15:59:35 +0545

    Update environment.ts

--- hamrokhata-ui/src/environments/environment.ts
+++ hamrokhata-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/api/',
+  apiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/api',
   publicApiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/api/public'
 };
