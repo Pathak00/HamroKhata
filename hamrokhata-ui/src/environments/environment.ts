fb58588e0ba8abb38655058e5c2e3a99bd3227c7
Rupak Pathak <pathakrupak00@gmail.com>
2026-05-02 15:54:13 +0545

    updated api for login

--- hamrokhata-ui/src/environments/environment.ts
+++ hamrokhata-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/',
-  publicApiUrl: 'http://localhost:5191/public'
+  apiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/api/',
+  publicApiUrl: 'https://hamrokhata-api-czgegtg7a0e3cqc8.southeastasia-01.azurewebsites.net/api/public'
 };
